b8e4aed9b3fe939e4d09a7b982bb1947339c9b7b
Lapidis2 <hitlapidis2@gmail.com>
2025-07-23 21:36:28 +0200

    change  welcome message

--- alx-project-0x03/tailwind.config.js
+++ alx-project-0x03/tailwind.config.js
@@ -1,9 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // or your actual paths
+  ],
+  safelist: [
+    'hover:bg-red-500/50',
+    'hover:bg-blue-500/50',
+    'hover:bg-orange-500/50',
+    'hover:bg-green-500/50',
+    'hover:bg-slate-500/50',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
