b70be563267fe00b20a5ed347995a7e4dfebe997
Lapidis2 <hitlapidis2@gmail.com>
2025-07-23 21:49:21 +0200

    Move your interfaces to the right directory

--- alx-project-0x03/tailwind.config.js
+++ alx-project-0x03/tailwind.config.js
@@ -1,17 +1,12 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // or your actual paths
-  ],
-  safelist: [
-    'hover:bg-red-500/50',
-    'hover:bg-blue-500/50',
-    'hover:bg-orange-500/50',
-    'hover:bg-green-500/50',
-    'hover:bg-slate-500/50',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	content: [
+	  "./pages/**/*.{js,ts,jsx,tsx}",
+	  "./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+  };
+  
